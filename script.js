9ed9c1a409f8c87cb07850099db22cd3ebc7e786
todor147 <todor_147@abv.bg>
2024-10-17 16:42:20 +0100

    Details inserted
    Details inserted and overall site improvements

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+let currentSlide = 0;
+
+function changeSlide(direction) {
+  const slides = document.querySelectorAll(".gallery-slide");
+  currentSlide = (currentSlide + direction + slides.length) % slides.length;
+  const offset = -currentSlide * 100;
+  document.querySelector(
+    ".gallery-container"
+  ).style.transform = `translateX(${offset}%)`;
 }
